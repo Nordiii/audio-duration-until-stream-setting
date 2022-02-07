8e486a7b0fa8a6f44739748d05d2037d3f3223dc
Nordiii <niklas907@googlemail.com>
2022-02-07 12:55:30 +0100

    update for v10

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,16 @@
 Hooks.once('init', async function() {
+    game.settings.register('SoundTimeToStreamSetting', 'durationToStream', {
+        name: "Time to Stream (seconds)",
+        hint: "The length of a song needed before streaming (default 600 seconds -> 10 min)",
+        config: true,
+        scope: "world",
+        type: Number,
+        default: 600,
+        onChange: (data) => {
+            AudioContainer.MAX_BUFFER_DURATION = data
+        }
+    });
 
-});
 
-Hooks.once('ready', async function() {
-
+    AudioContainer.MAX_BUFFER_DURATION = game.settings.get("SoundTimeToStreamSetting", "durationToStream")
 });
