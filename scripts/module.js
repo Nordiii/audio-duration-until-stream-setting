41e3acd8aa6572c0c42462a6ffaae1afddd6aabc
Nordiii <niklas907@googlemail.com>
2022-02-20 20:51:24 +0100

    fix name - need reinstall
    Revert "add settings menu"
    
    This reverts commit ca9af9fc8831ead296aa3218703cf88c47e9c084.
    
    Revert "add settings menu"
    
    This reverts commit 7edb00ce7da6f5fe10ed5b22d17f61faf028c87f.

--- scripts/module.js
+++ scripts/module.js
@@ -1,16 +1,3 @@
 Hooks.once('init', async function() {
-    game.settings.register('SoundTimeToStreamSettings', 'durationToStream', {
-        name: "Time to Stream",
-        hint: "The length of a song needed before streaming (default 600 seconds/10 min)",
-        config: true,
-        scope: "world",
-        type: Number,
-        default: 600,
-        onChange: (data) => {
-            AudioContainer.MAX_BUFFER_DURATION = data
-        }
-    });
-
-
-    AudioContainer.MAX_BUFFER_DURATION = game.settings.get("sttss", "durationToStream")
+    AudioContainer.MAX_BUFFER_DURATION = 30
 });
