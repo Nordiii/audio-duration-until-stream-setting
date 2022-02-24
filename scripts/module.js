8c031de8e9a10dfbab4e5263f2a49b471ba22013
Nordiii <niklas907@googlemail.com>
2022-02-24 22:29:31 +0100

    Revert "fix name - need reinstall"
    This reverts commit 41e3acd8aa6572c0c42462a6ffaae1afddd6aabc.

--- scripts/module.js
+++ scripts/module.js
@@ -1,3 +1,16 @@
 Hooks.once('init', async function() {
-    AudioContainer.MAX_BUFFER_DURATION = 30
+    game.settings.register('SoundTimeToStreamSettings', 'durationToStream', {
+        name: "Time to Stream",
+        hint: "The length of a song needed before streaming (default 600 seconds/10 min)",
+        config: true,
+        scope: "world",
+        type: Number,
+        default: 600,
+        onChange: (data) => {
+            AudioContainer.MAX_BUFFER_DURATION = data
+        }
+    });
+
+
+    AudioContainer.MAX_BUFFER_DURATION = game.settings.get("sttss", "durationToStream")
 });
